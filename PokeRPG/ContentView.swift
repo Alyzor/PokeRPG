ff1ecafaee8f66c3a07eede1ee7d7f4ea5a85cdf
Pedro Pereira <pereirapedro591@gmail.com>
2023-06-07 00:42:46 +0100

    Initial Commit

---
+++ PokeRPG/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeRPG
+//
+//  Created by Account1 on 07/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokeRPG/PokeRPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeRPGApp.swift
+//  PokeRPG
+//
+//  Created by Account1 on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PokeRPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
